28d3a0e16061c3494b0e283755cefc1434ee5fe2
cyKim <76chanyoung@gmail.com>
2025-05-14 14:05:48 +0900

    테스트 씬과 코드 구성

--- Assets/GoogleSpreadSheetLoader/Runtime/LocalizeTable.cs
+++ Assets/GoogleSpreadSheetLoader/Runtime/LocalizeTable.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using UnityEngine;
-// ReSharper disable MemberCanBePrivate.Global
 
 public static class LocalizeTable
 {
--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -1,12 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using TableData;
 using UnityEngine;
-using GoogleSpreadSheetLoader;
+using TMPro;
 
 public class TestScript : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private TMP_InputField inputFieldDataId;
+    [SerializeField] private TMP_Text txtDataValue;
+    
+    [Space(5)]
+    [SerializeField] private SystemLanguage language;
+    [SerializeField] private TMP_InputField inputFieldLocalizeKey;
+    [SerializeField] private TMP_InputField inputFieldLocalizeParam1;
+    [SerializeField] private TMP_InputField inputFieldLocalizeParam2;
+    [SerializeField] private TMP_Text txtLocalizeValue;
+
+    private TableLinker _tableLinker;
+    
+    private void Start()
     {
-        GSSL_EditorWindow.ShowWindow();
+        _tableLinker = Resources.Load<TableLinker>("TableLinker");
+    }
+
+    public void OnClickData()
+    {
+        // inputFieldDataId.text;
+        // _tableLinker.StageTable.dataList.Find(x=>x.ID == )
+    }
+    
+    public void OnClickLocalizeInitialize()
+    {
+        LocalizeTable.Initialize(language);
+    }
+
+    public void OnClickLocalize()
+    {
+        var localizeKey = inputFieldLocalizeKey.text;
+        var param1 = inputFieldLocalizeParam1.text;
+        var param2 = inputFieldLocalizeParam2.text;
+        
+        txtLocalizeValue.text = localizeKey.GetLocalizeText(param1, param2);
     }
 }
